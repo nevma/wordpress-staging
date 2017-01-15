a56e0d813abcd89a1f5332a4f860c984cfba0df1
Takis Bouyouris <takis@nevma.gr>
2017-01-15 02:40:19 +0200

    Removed script config file again

--- dist/bin/wp-staging-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# The directory where the staging websites will be placed
-BASE_DIRECTORY=/home/staging/public_html
-
-# The url by which the base directory is exposed to the internet
-BASE_URL=http://staging.mydomain.com

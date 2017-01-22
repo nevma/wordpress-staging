8fe16a6b9ce5b754669bb66eb97378a1ecef5745
Takis Bouyouris <takis@nevma.gr>
2017-01-22 18:11:14 +0200

    Minor update in default config file.

--- app/script/dist/bin/wp-staging-config.sh
+++ app/script/dist/bin/wp-staging-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # The directory where the staging websites will be placed
-BASE_DIRECTORY=<BASE_DIRECTORY_FULL_PATH>
+BASE_DIRECTORY=<BASE_DIRECTORY_ABSOLUTE_PATH>
 
 # The url by which the base directory is exposed to the internet
 BASE_URL=<BASE_URL_FULL_PATH>

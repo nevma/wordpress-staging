4fe18646444816d94857e5ace0a8138448891633
Takis Bouyouris <takis@nevma.gr>
2019-02-15 19:24:13 +0200

    Corrected defailt settings file.

--- app/inc/settings.php
+++ app/inc/settings.php
@@ -1,9 +1,9 @@
 <?php
 
     // The base url from which all the staging websites will be accessible.
-    define( 'BASE_URL', 'http://clients.nevma.gr' );
+    define( 'BASE_URL', 'http://staging.domain.com/path' );
 
     // The base directory where all the staging websites will be copied.
-    define( 'BASE_DIRECTORY', '/home/nevmaclients/public_html' );
+    define( 'BASE_DIRECTORY', '/path/to/staging/public_html' );
 
 ?>
